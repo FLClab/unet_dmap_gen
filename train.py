8bee533814f0d7be316245788fe1e960abcd3543
Benoit Turcotte <benoiturcotte@gmail.com>
2022-06-28 15:35:04 -0400

    :)

--- train.py
+++ train.py
@@ -284,9 +284,13 @@ if __name__ == "__main__":
     print(f"Length of file names for training = {len(files_training)}")
     print(f"Length of file names for validation = {len(files_testing)}")
     print("---------------")
+    print("training :")
     for i, name in enumerate(files_training):
+        print(f"i = {i}, name = {name}")
         os.rename(os.path.join(training_path, name), os.path.join(training_path, f"{str(i)}"))
+    print("testing :")
     for i, name in enumerate(files_testing):
+        print(f"i = {i}, name = {name}")
         os.rename(os.path.join(testing_path, name), os.path.join(testing_path, f"{str(i)}"))
     files_training_valid = [name for name in os.listdir(training_path) if os.path.isfile(os.path.join(training_path, name))]
     files_testing_valid = [name for name in os.listdir(testing_path) if os.path.isfile(os.path.join(testing_path, name))]
